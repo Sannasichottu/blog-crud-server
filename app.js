d1c691d775f55235dcd8bb643d1b144a137f0e15
sannasichottu <sannasichottu@gmail.com>
2022-08-13 18:42:49 +0530

    PORT added

--- app.js
+++ app.js
@@ -12,12 +12,12 @@ app.use(express.json());
 
 //User
 app.use("/api/user",router);
-
+const PORT = process.env.PORT || 5000;
 //Blog
 app.use("/api/blog",blogRouter)
 mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@clusters.k8lwf.mongodb.net/blogcurd?retryWrites=true&w=majority`)
-.then(() => app.listen(5000))
+.then(() => app.listen(PORT))
 .then(() => 
-console.log("Connected to Database and Listening to Loalhost 5000")
+console.log(`Connected to Database and Listening to Loalhost ${PORT}`)
 )
 .catch((err) => console.log(err));
