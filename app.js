5f0fb1105308df473ffc645c7ecd444d39b31a75
sannasichottu <sannasichottu@gmail.com>
2022-08-13 11:59:35 +0530

    dotenv added

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ import mongoose from 'mongoose';
 import blogRouter from "./routes/blog-routes";
 import router from "./routes/user-routes";
 import cors from 'cors';
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
@@ -13,8 +15,7 @@ app.use("/api/user",router);
 
 //Blog
 app.use("/api/blog",blogRouter)
-
-mongoose.connect('mongodb+srv://blogApp:Dhosanjay7@clusters.k8lwf.mongodb.net/blogcurd?retryWrites=true&w=majority')
+mongoose.connect(`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PW}@clusters.k8lwf.mongodb.net/blogcurd?retryWrites=true&w=majority`)
 .then(() => app.listen(5000))
 .then(() => 
 console.log("Connected to Database and Listening to Loalhost 5000")
